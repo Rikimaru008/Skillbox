974a5f084571a8f32f84ccfa0d67fadd4a7ce472
Rikimaru008 <78591729+Rikimaru008@users.noreply.github.com>
2023-02-02 22:06:26 +0300

    Update CLI3.sh

--- CLI3.sh
+++ CLI3.sh
@@ -5,7 +5,16 @@ read -p "напишите disk_id backup:" backup
 read -p "напишите disk_id openvpn:" openvpn
 read -p "напишите disk_id monitoring:" monitoring
 
+yc compute instance stop backup
+yc compute instance stop openvpn
+yc compute instance stop monitoring
+
 
 yc compute disk resize $backup --size 18
 yc compute disk resize $openvpn --size 18
 yc compute disk resize $monitoring --size 18
+
+
+yc compute instance start backup
+yc compute instance start openvpn
+yc compute instance start monitoring
